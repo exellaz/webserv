4911b18b0921f589ace1ec6cadb94d26ea9c00e6
Samuel Ting <sting@u82z03s01.42kl.edu.my>
2025-05-30 14:55:43 +0800

    refactor: groouped receivedClientData into a func

--- src/sockets-polling/main.cpp
+++ src/sockets-polling/main.cpp
@@ -11,7 +11,7 @@ void acceptClient(std::vector<struct pollfd>& pfds, int listener)
 	else {
 		
 		// server sends text to every client that connects
-		const char *hello = "Hello from server!";
+		const char *hello = "Hello from server!\n";
 		send(newFd, hello, strlen(hello), 0);
 
 		addToPfds(pfds, newFd);
@@ -26,10 +26,29 @@ void acceptClient(std::vector<struct pollfd>& pfds, int listener)
 	}
 }
 
-// void receiveDataFromClient()
-// {
-//
-// }
+void receiveClientData(struct pollfd& pfd)
+{
+	char buf[3000];    // Buffer for client data
+	int nBytes = recv(pfd.fd, buf, sizeof buf, 0);
+	buf[nBytes] = '\0';
+	int senderFd = pfd.fd;
+
+	if (nBytes <= 0) {
+		// Got error or connection closed by client
+		if (nBytes == 0) {
+			// Connection closed
+			std::cout << "pollserver: socket " << senderFd << " hung up\n"; 
+		} else {
+			perror("recv");
+		}
+	} else {
+		// do something with client data
+		std::cout << "Data from Client: \n"<< buf << '\n';
+
+		const std::string response = "Response from server\n";
+		send(pfd.fd, response.c_str(), response.size(), 0);
+	}
+}
 
 
 // Main
@@ -44,7 +63,6 @@ int main(void)
     while(1) {
 		
 		std::cout << "\n+++++++ Waiting for new connection ++++++++\n\n";
-		// NOTE: POLL
         // wait until 1 or more fds become ready for reading (POLLIN) or other events.
         int pollCount = poll(&pfds[0], pfds.size(), -1);
         if (pollCount == -1) {
@@ -64,46 +82,11 @@ int main(void)
 
                 if (pfds[i].fd == listener)
 					acceptClient(pfds, listener);
-                else {
-					// NOTE: Receive data from Client
-
-                    // If not the listener, we're just a regular client
-					char buf[3000];    // Buffer for client data
-                    int nBytes = recv(pfds[i].fd, buf, sizeof buf, 0);
-					buf[nBytes] = '\0';
-                    int senderFd = pfds[i].fd;
-
-                    if (nBytes <= 0) {
-                        // Got error or connection closed by client
-                        if (nBytes == 0) {
-                            // Connection closed
-							std::cout << "pollserver: socket " << senderFd << " hung up\n"; 
-                        } else {
-                            perror("recv");
-                        }
-                        close(pfds[i].fd); // Bye!
-						delFromPfds(pfds, i);
-                        i--;
-                    } else {
-						// do something with client data
-						std::cout << "Data from Client: \n"<< buf << '\n';
-
-						const char* htmlBody = "<!DOCTYPE html><html><body><h1>Hello from your C++ server!</h1></body></html>";
-						int contentLength = strlen(htmlBody);
-						std::ostringstream oss;
-						oss << "HTTP/1.1 200 OK\r\n"
-							<< "Content-Type: text/html\r\n"
-							<< "Content-Length: " << contentLength << "\r\n"
-							<< "Connection: close\r\n"
-							<< "\r\n"
-							<< htmlBody;
-						std::string httpResponse = oss.str();
-						send(pfds[i].fd, httpResponse.c_str(), httpResponse.size(), 0);
-
-						close(pfds[i].fd);
-						delFromPfds(pfds, i);
-						i--;
-					}
+                else { // Client socketfd
+					receiveClientData(pfds[i]);
+					close(pfds[i].fd);
+					delFromPfds(pfds, i);
+					i--;
                 } 
             } 
         } 
