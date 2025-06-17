31a1a8acc1bd4a3eebf4c69b2d4c4454324ba041
low <lowwenjuin27@gmail.com>
2025-06-18 03:25:34 +0800

    fix: handle check for connection.fd is it similar to pdfs.fd

--- src/main.cpp
+++ src/main.cpp
@@ -39,6 +39,18 @@ void sendResponseToClient(int fd, HttpResponse& response)
     std::cout << "server: Response sent to client.\n";
 }
 
+/**
+ * @brief check the pdfs.fd is similar to the connection.fd
+*/
+std::vector<Connection>::iterator CheckConnection(std::vector<Connection> &connections, int fd)
+{
+    for (std::vector<Connection>::iterator it = connections.begin(); it != connections.end(); ++it) {
+        if (it->fd == fd)
+            return it;
+    }
+    return connections.end();
+}
+
 int main(int argc, char **argv)
 {
     std::string configFile;
@@ -56,38 +68,38 @@ int main(int argc, char **argv)
         configFile = argv[1];
     }
 
-	try {
-		std::vector<Config> configs = parseAllServers(configFile);
+    try {
+        std::vector<Config> configs = parseAllServers(configFile);
 
         std::vector<struct pollfd> pfds;
-	    std::vector<Connection> connections;
-		std::vector<int> listeners;
-		std::map<int, std::string> listenerToConfig;
+        std::vector<Connection> connections;
+        std::vector<int> listeners;
+        std::map<int, std::string> listenerToConfig;
 
         // listener Socket Fd
         //int listener = setupListeningSocket(pfds, connections, configs);
-		for (size_t i = 0; i < configs.size(); ++i)
-		{
-			if (configs[i].getPort().empty() || configs[i].getHost().empty())
-				continue;
-			int listener = setupListeningSocket(pfds, connections, configs[i]);
-			listeners.push_back(listener);
-		}
+        for (size_t i = 0; i < configs.size(); ++i)
+        {
+            if (configs[i].getPort().empty() || configs[i].getHost().empty())
+                continue;
+            int listener = setupListeningSocket(pfds, connections, configs[i]);
+            listeners.push_back(listener);
+        }
 
         while(1) {
 
             std::cout << CYAN << "\n+++++++ Waiting for new connection ++++++++" << RESET << "\n\n";
 
-			// TODO: calculate nearest Timeout among all connections, assign to poll()
+            // TODO: calculate nearest Timeout among all connections, assign to poll()
 
             // wait until 1 or more fds become ready for reading (POLLIN) or other events.
-			int nearestTimeout = getNearestUpcomingTimeout(connections);
+            int nearestTimeout = getNearestUpcomingTimeout(connections);
             int pollCount = poll(&pfds[0], pfds.size(), nearestTimeout);
             if (pollCount == -1) {
                 throw PollErrorException();
             }
 
-			disconnectTimedOutClients(connections, pfds);
+            disconnectTimedOutClients(connections, pfds);
 
             // Run through the existing connections looking for data to read
             for(size_t i = 0; i < pfds.size(); i++) {
@@ -95,38 +107,75 @@ int main(int argc, char **argv)
                 if (pfds[i].revents & (POLLIN )) {
                     std::cout << "POLLIN\n";
 
-					//look for multiple listeners
+                    // If not a listener socket, then it's a client socket
+
+                    //look for multiple listeners
                     if (find(listeners.begin(), listeners.end(), pfds[i].fd) != listeners.end())
                         acceptClient(pfds, connections, pfds[i].fd);
-                    else {
-						//int res = receiveClientRequest(connections[i]);
-						int res = receiveClientRequest(connections[i], configs); //pass multiple configs
-						if (res == RECV_CLOSED) {
-							std::cout << "server: socket " << pfds[i].fd << " hung up\n";
-							disconnectClient(connections, pfds, i);
-							i--;
-							continue;
-						}
-						// connections[i].isResponseReady = true; // HARDCODED
-						if (connections[i].isResponseReady)
-							pfds[i].events |= POLLOUT;
+                    else
+                    {
+                        //int res = receiveClientRequest(connections[i]);
+                        //int res = receiveClientRequest(connections[i], configs); //pass multiple configs
+                        //    if (res == RECV_CLOSED) {
+                        //        std::cout << "server: socket " << pfds[i].fd << " hung up\n";
+                        //        disconnectClient(connections, pfds, i);
+                        //        i--;
+                        //        continue;
+                        //    }
+                        //    // connections[i].isResponseReady = true; // HARDCODED
+                        //    if (connections[i].isResponseReady)
+                        //        pfds[i].events |= POLLOUT;
+
+                        // Find the connection corresponding to the current fd
+                        //TODO check the connection fd
+                        std::vector<Connection>::iterator connectionFd = CheckConnection(connections, pfds[i].fd);
+                        if (connectionFd != connections.end())
+                        {
+                            int res = receiveClientRequest(connections[i], configs); //pass multiple configs
+                            if (res == RECV_CLOSED) {
+                                std::cout << "server: socket " << pfds[i].fd << " hung up\n";
+                                disconnectClient(connections, pfds, i);
+                                i--;
+                                continue;
+                            }
+                            // connections[i].isResponseReady = true; // HARDCODED
+                            if (connections[i].isResponseReady)
+                                pfds[i].events |= POLLOUT;
+                        }
                     }
                 }
                 else if (pfds[i].revents & POLLOUT) {
-                    std::cout << "POLLOUT\n";
+					std::vector<Connection>::iterator connectionFd = CheckConnection(connections, pfds[i].fd);
+					if (connectionFd != connections.end())
+					{
+						std::cout << "POLLOUT\n";
+                    	sendResponseToClient(connections[i].fd, connections[i].response);
+                    	// pfds[i].events |= POLLIN;
+                    	connections[i].isResponseReady = false;
+                    	connections[i].request.clearRequest();
+                    	connections[i].response.clearResponse();
 
-                    sendResponseToClient(connections[i].fd, connections[i].response);
-                    // pfds[i].events |= POLLIN;
-                    connections[i].isResponseReady = false;
-                    connections[i].request.clearRequest();
-                    connections[i].response.clearResponse();
+                    	if (connections[i].connType == CLOSE) {
+                    	    disconnectClient(connections, pfds, i);
+                    	    i--;
+                    	    continue;
+                    	}
+                    	pfds[i].events &= ~POLLOUT;
+					}
+                    //std::cout << "POLLOUT\n";
 
-                    if (connections[i].connType == CLOSE) {
-                        disconnectClient(connections, pfds, i);
-                        i--;
-                        continue;
-                    }
-                    pfds[i].events &= ~POLLOUT;
+                    //sendResponseToClient(connections[i].fd, connections[i].response);
+                    //// pfds[i].events |= POLLIN;
+                    //connections[i].isResponseReady = false;
+                    //connections[i].request.clearRequest();
+                    //connections[i].response.clearResponse();
+
+                    //if (connections[i].connType == CLOSE) {
+                    //    disconnectClient(connections, pfds, i);
+                    //    i--;
+                    //    continue;
+                    //}
+                    //pfds[i].events &= ~POLLOUT;
                 }
                 else if (pfds[i].revents & POLLHUP) {
                     std::cout << "POLLHUP\n";
