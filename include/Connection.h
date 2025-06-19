dfcd891c6c0620e456c73538d133d7623d69deed
low <lowwenjuin27@gmail.com>
2025-06-19 13:19:12 +0800

    fix: debug buffer size

--- include/Connection.h
+++ include/Connection.h
@@ -37,7 +37,7 @@ public:
 	size_t contentLength;
 	bool isResponseReady;
 
-	void appendToBuffer(char *str);
+	void appendToBuffer(char* str);
 	const std::string& getBuffer() const;
 	void setBuffer(std::string str);
 	void clearBuffer();
